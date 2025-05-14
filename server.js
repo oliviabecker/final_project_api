3f8c58243a283bd1973e936dcf2bbaa0484a9ce7
Olivia Becker <o_becker@mail.fhsu.edu>
2025-05-14 13:39:54 -0500

    Changed port to 3600

--- server.js
+++ server.js
@@ -1,38 +1,19 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
-require('dotenv').config();
+const PORT = process.env.PORT || 3600;
 
-const PORT = process.env.PORT || 3500;
+// Serve static files if needed
+app.use(express.static('public'));
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Static HTML files (for root and 404)
-app.use(express.static(path.join(__dirname, 'views')));
-
-
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://ocbecker24:NrfWliJwnQqeQsR1@clusterapi.sz6d8wr.mongodb.net/statesDB?retryWrites=true&w=majority')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
-
-// Routes
-app.use('/states', require('./routes/states'));
-
-// Root Route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  });
-
-// 404 handler (for non-API routes)
-app.all('*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+// Root: Serve index.html
+app.get('^/$|/index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Start server
-app.listen
+// 404: Serve 404.html for any unknown route
+app.all('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
